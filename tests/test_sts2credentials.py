910558032be50fc09296a7792b16dbfa89468c9d
ynouri <ynouri@users.noreply.github.com>
2020-05-31 20:37:08 -0400

    Fix linting

--- tests/test_sts2credentials.py
+++ tests/test_sts2credentials.py
@@ -1,5 +1,10 @@
+"""
+Tests for the sts2credentials package
+"""
+
 from sts2credentials.__main__ import configure_credentials
 
 
 def test_configure_credentials():
+    """Test the configuration step"""
     configure_credentials()
